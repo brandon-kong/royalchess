1635921453a0093ef089df57525684660ccfc372
Brandon Kong <kongbrandon0@gmail.com>
2024-05-05 18:40:39 -0500

    storybook: added dark theme

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
 import "@repo/ui/styles.css";
 
@@ -10,6 +11,9 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
