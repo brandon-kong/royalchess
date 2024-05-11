dfecff0637104cc2109271509d55a4327a8ef45a
Brandon Kong <kongbrandon0@gmail.com>
2024-05-10 23:58:07 -0500

    style: formatted

--- apps/backend/core/settings.py
+++ apps/backend/core/settings.py
@@ -265,11 +265,11 @@ CORS_ALLOWED_ORIGINS = [
 
 # Email
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'admin@localhost'
-EMAIL_HOST = 'localhost'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "admin@localhost"
+EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
